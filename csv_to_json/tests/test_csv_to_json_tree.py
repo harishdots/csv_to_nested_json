1032ab6ed08fad30bcb6c53c394b9419b7babbd1
Harish Kumar <harish.kumar@dotsquares.com>
2021-10-19 12:21:38 +0530

    pep8 code formating with using proper docstring

--- csv_to_json/tests/test_csv_to_json_tree.py
+++ csv_to_json/tests/test_csv_to_json_tree.py
@@ -3,30 +3,32 @@ from io import BytesIO
 import json
 import pandas as pd
 
-
 from csv_to_json.csv_to_json_tree import ConvertCsvToJsonTree
-
 
 RESULT = False
 ALLOWED_EXTENSIONS = {'csv'}
 
+
 class FileObj(BytesIO):
-            
+
     def close(self):
         global RESULT
         RESULT = True
+
 
 class MyRequest(Request):
     def _get_file_stream(*args, **kwargs):
         return FileObj()
 
+
 app = Flask(__name__)
 app.debug = True
 app.request_class = MyRequest
 
+
 def allowed_file(filename):
     return '.' in filename and \
-    filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 class TestConvertCsvToNestedJson:
@@ -44,31 +46,30 @@ class TestConvertCsvToNestedJson:
                 assert isinstance(
                     csv_file.stream,
                     FileObj,
-                )                
+                )
                 return 'ok'
-        
+
         client = app.test_client()
         resp = client.post(
             '/upload', buffered=True,
-            data = {
+            data={
                 'file': (BytesIO(b'FILE CONTENT'), 'sample_data.csv'),
             },
             content_type='multipart/form-data'
         )
 
         assert b'ok' == resp.data
-    
 
     def test_create_nested_tree(self):
 
-        # Read uploade csv file data using pandas lib.
+        # Read upload csv file data using pandas lib.
         csv_data = pd.read_csv("csv_to_json/tests/sample_data.csv")
         # Remove all empty data
         csv_data = csv_data.dropna(how='all')
 
         """ Class ConvertCsvToJsonTree is used to create nested json data """
         csv_object = ConvertCsvToJsonTree(csv_data)
-        out_json = csv_object.create_parent_tree(is_dump_with_indent = False)
+        out_json = csv_object.create_parent_tree(is_dump_with_indent=False)
 
         with open('csv_to_json/tests/output.json', 'r') as json_file:
             out_data = json.load(json_file)
